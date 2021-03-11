be91cb13ece0245228397178cc645166213498fa
João Meireles <jvmeireles08@gmail.com>
2021-03-11 14:58:21 -0300

    chromedriver added

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 url = 'https://blaze.com/pt/games/crash'
 
 option = Options()
 option.headless = False
-driver = webdriver.Firefox(options=option)
+driver = webdriver.Chrome(options=option)
 
 driver.get(url)
 driver.quit()
